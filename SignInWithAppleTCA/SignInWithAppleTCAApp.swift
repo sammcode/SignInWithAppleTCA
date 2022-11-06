2c54e148fd783cf74569d1d230dff1fb448fdc5e
Samuel McGarry <samuel.mcgarry@gmail.com>
2022-11-06 17:50:10 -0500

    Initial Commit

---
+++ SignInWithAppleTCA/SignInWithAppleTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleTCAApp.swift
+//  SignInWithAppleTCA
+//
+//  Created by Samuel McGarry on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
